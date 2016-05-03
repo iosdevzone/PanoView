fba23d649ab0640597a22334a320275c40ce6222
iosdevzone <idz@iosdeveloperzone.com>
2016-05-02 20:09:57 -0700

    Completed tutorial.

--- PanoView/ViewController.swift
+++ PanoView/ViewController.swift
@@ -21,6 +21,65 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Load assets
+        guard let imagePath = NSBundle.mainBundle().pathForResource("Hellbrunn25", ofType: "jpg") else {
+            fatalError("Failed to find path for panaromic file.")
+        }
+        guard let image = UIImage(contentsOfFile:imagePath) else {
+            fatalError("Failed to load panoramic image")
+        }
+        
+        // Set the scene
+        let scene = SCNScene()
+        sceneView.scene = scene
+        sceneView.showsStatistics = true
+        sceneView.allowsCameraControl = true
+        
+        //Create node, containing a sphere, using the panoramic image as a texture
+        let sphere = SCNSphere(radius: 20.0)
+        sphere.firstMaterial!.doubleSided = true
+        sphere.firstMaterial!.diffuse.contents = image
+        let sphereNode = SCNNode(geometry: sphere)
+        sphereNode.position = SCNVector3Make(0,0,0)
+        scene.rootNode.addChildNode(sphereNode)
+        
+        // Lights, ...
+        let ambientLightNode = SCNNode()
+        ambientLightNode.light = SCNLight()
+        ambientLightNode.light!.type = SCNLightTypeAmbient
+        ambientLightNode.light!.color = UIColor(white: 0.67, alpha: 1.0)
+        scene.rootNode.addChildNode(ambientLightNode)
+        
+        // Camera, ...
+        cameraNode.camera = SCNCamera()
+        cameraNode.position = SCNVector3Make(0, 0, 0)
+        scene.rootNode.addChildNode(cameraNode)
+        
+        // Action (Sorry! I could not help myself)
+        if motionManager.deviceMotionAvailable {
+            motionManager.deviceMotionUpdateInterval = 1.0 / 60.0
+            motionManager.startDeviceMotionUpdatesToQueue(NSOperationQueue.mainQueue()) {
+                [weak self](data: CMDeviceMotion?, error: NSError?) in
+                
+                guard let data = data else {
+                    NSLog("Error in deviceMotionUpdate \(error)")
+                    return
+                }
+                guard let strongSelf = self else {
+                    NSLog("Captured weak self was nil in deviceMotionUpdate")
+                    return
+                }
+                
+                let attitude: CMAttitude = data.attitude
+                
+                strongSelf.cameraNode.eulerAngles = SCNVector3Make(Float(attitude.roll - M_PI/2.0), Float(attitude.yaw), Float(attitude.pitch))
+            }
+            
+        }
+        else {
+            fatalError("Device motion is not available")
+        }
+
 
     }
 
